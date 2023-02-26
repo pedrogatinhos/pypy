bb9cf1b8ab03b5cdf6f65ccdf9719cf600db34b3
pedrogatinhos <pedrohenriquebtcc@gmail.com>
2023-02-26 10:02:15 -0300

    super! - dicionarios

--- syntax/class0object.py
+++ syntax/class0object.py
@@ -1,4 +1,4 @@
-class minhaclasse:
+class Pessoa:
     def __init__(self, nome, sobrenome):
         self.nome = nome
         self.sobrenome = sobrenome
@@ -8,6 +8,16 @@ class minhaclasse:
         return str(self.__dict__)
    
 
-primeiro = minhaclasse('pedro', 'henrique')
+primeiro = Pessoa('pedro', 'henrique')
 print(primeiro)
 primeiro.cumprimentar()
+
+class Aluno(Pessoa):
+    def __init__(self, nome, sobrenome, curso):
+        super().__init__(nome, sobrenome)
+        self.curso = curso
+    def __repr__333(self):
+        return super().__repr__()
+    
+segundo = Aluno('pedro','henrique', 'S.I')
+print(segundo)
