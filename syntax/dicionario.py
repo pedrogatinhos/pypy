2b7bc23350d957f2db3cfd1700b0d4132455ebf5
pedrogatinhos <pedrohenriquebtcc@gmail.com>
2023-02-26 10:02:15 -0300

    super! and dics

---
+++ syntax/dicionario.py
@@ -0,0 +1,16 @@
+dic = {"nome": "pedro", "idade": "20", "cidade": "macaé"}
+print(dic)
+
+print(dic.keys())
+print(dic.values())
+print(dic.items())
+
+print(dic["cidade"])
+
+dic["campoextra"] = "nenhum"
+
+print(dic)
+
+dic["campoextra"] = "nenhum2"
+
+print(dic)
---
+++ syntax/set.py
@@ -0,0 +1,5 @@
+conjunto = {1,1,1,1,2}
+print(conjunto)
+
+# set = set(1)
+# print(set)
